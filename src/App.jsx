4bb9c3e7ebbc5a52f519c9caaa947b57092985f7
Andreas Gramstad <andreasgramstad@Andreass-MacBook-Pro-2.local>
2025-05-30 15:25:26 +0200

    Implement navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,19 @@
 import { Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import ProjectDetail from './pages/ProjectDetail';
+import Navbar from './components/Navbar';
 
 const App = () => {
   return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/projects/:id" element={<ProjectDetail />} />
-    </Routes>
+    <>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/projects/:id" element={<ProjectDetail />} />
+      </Routes>
+    </>
   );
 };
+
 
 export default App;
